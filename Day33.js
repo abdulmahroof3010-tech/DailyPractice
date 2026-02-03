fe2a26b9be340800b320e21e74ba68379b4f9363
Your Name <your_email@example.com>
2026-02-03 10:21:49 +0530

    practiced

---
+++ Day33.js
@@ -0,0 +1,13 @@
+function greeting(name){
+
+  if(name=="Mubashir"){
+    return "Hello, my Love!"
+  }
+    return "Hello, ".concat(name,"!")
+    
+
+}
+
+console.log(greeting("Matt"))
+console.log(greeting("Helen"))
+console.log(greeting("Mubashir"))
