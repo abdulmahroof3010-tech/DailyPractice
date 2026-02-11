ffc551e47146d04c5c570d9dfc23e1af94c5f43f
Your Name <abdulmahroof3010@gmail.com>
2026-02-11 10:02:45 +0530

    practiced

---
+++ Day34.js
@@ -0,0 +1,13 @@
+function makeTen(a,b){
+    let c=a+b
+     if(a===10 || b===10 || c===10){
+        return true
+     }
+     return false
+
+
+}
+
+console.log(makeTen(9,10))
+console.log(makeTen(9,9))
+console.log(makeTen(1,10))
---
+++ Day36.js
@@ -0,0 +1,12 @@
+function maxNum(num1,num2){
+
+    if(num1<num2){
+        return num2
+    }
+    return num1
+
+}
+
+console.log(maxNum(3,7))
+console.log(maxNum(-1,0))
+console.log(maxNum(1000,400))
