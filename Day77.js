76c15afc946cefbedbc3c530d1088ece7fb3d203
Your Name <abdulmahroof3010@gmail.com>
2026-02-24 08:41:41 +0530

    practiced

---
+++ Day77.js
@@ -0,0 +1,23 @@
+function countOfNumber(n){
+    n=Math.abs(n)
+
+    if(n===0) return 0
+
+    let count=0;
+
+    while(n>0){
+        n=Math.floor(n/10);
+        count++
+    }
+    return count
+}
+console.log(countOfNumber(4))
+console.log(countOfNumber(14))
+console.log(countOfNumber(234))
+console.log(countOfNumber(-234))
+console.log(countOfNumber(0))
+
+
+
+
+
---
+++ Day78.js
@@ -0,0 +1,12 @@
+function starPattern(n){
+
+    for(let i=0;i<n;i++){
+        let row="";
+        for(let j=0;j<=i;j++){
+            row+="*"
+        }
+        console.log(row)
+    }
+
+}
+starPattern(5)
