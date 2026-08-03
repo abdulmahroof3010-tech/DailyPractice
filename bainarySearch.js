191fdebed07315963bfc5637a1574382d1e25174
Your Name <abdulmahroof3010@gmail.com>
2026-08-03 12:39:55 +0530

    solved the pattern22 problem

---
+++ bainarySearch.js
@@ -0,0 +1,21 @@
+function binarySearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
+    
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(arr[mid]===target){
+            return mid
+     }else if(arr[mid]>target){
+            right=mid-1;
+      }else{
+        left=mid+1
+      }
+
+    }
+
+
+
+}
+
+console.log(binarySearch([1,8,9,10], 9))
---
+++ pattern22.js
@@ -0,0 +1,28 @@
+function pattern22(n){
+
+    for(let i=0;i<2*n-1;i++){
+        let row="";
+        for(let j=0;j<2*n-1;j++){
+            let last=2*n-2;
+            let top=i;
+            let left=j;
+            let bottom=last-i;
+            let right=last-j;
+           let min=top <left ?top  :left;
+           min=min<bottom?min:bottom;
+           min=min<right?min:right
+
+
+           let value=n-min;
+           row+=value+" "
+
+        }
+
+        console.log(row)
+
+    }
+
+}
+
+pattern22(5);
+// pattern22(4);
